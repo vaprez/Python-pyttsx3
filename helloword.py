1c5467a38814cff9ba876cb0f09fe5081e1d9752
vaprez <84541629+vaprez@users.noreply.github.com>
2021-05-22 23:24:38 +0000

    Update helloword.py

--- helloword.py
+++ helloword.py
@@ -1,17 +1,27 @@
+import speech_recognition as sp
 import pyttsx3
 import os
 
 pyttsx3.speak ("Hello my name is lucie")
 print("Hello my name is lucie")
-#ceci est un commentaire 
+#ceci est un commentaire
 pyttsx3.speak ("what is your name")
+#Ici nous allons entrez(ecrit) notre nom et l'ordinateur va repondre en appelans notre nom
 print("Enter your name:")
 x = input()
 print("Hello, " + x)
 pyttsx3.speak ("Hello, " + x)
-#ceci est un commentaire 
 pyttsx3.speak ("what can i do for you" + x)
-y = input()
+#Ici nous allons dire(de maniere vocal) a notre ordinateur d'executer une instruction par exemple lui dire "lucie open firefox" pour cela nous allons attendre qu'il affiche le message "wait for dpeach" avant de parler
+r = sp.Recognizer()
+with sp.Microphone() as source:
+    print("Wait for speach")
+    output = r.listen(source)
+    print("done")
+
+data = r.recognize_google(output)
+
+print(data)
 if"firefox" in y :
   os.system("start firefox")
 elif"chrome" in y :
